7826ddfe8474c74e8c7138ee4d96e373da42ba72
kamus <lukaseinsteinhawking@gmail.com>
2021-04-09 16:25:52 -0300

    Writing your first Django app, part 4

--- polls/views.py
+++ polls/views.py
@@ -10,8 +10,11 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 # Leave the rest of the views (detail, results, vote) unchanged
 def detail(request, question_id):
